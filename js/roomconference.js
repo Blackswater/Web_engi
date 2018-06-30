a4a742bbb99556150b4ca710799f5775ca48868d
Blackswater <Raphaellawo@gmx.de>
2018-06-30 16:38:58 +0200

    Javascript

--- js/roomconference.js
+++ js/roomconference.js
@@ -1,3 +1,21 @@
+
+
+var tagArray = { "status": "ok", "tags":[
+    {"id":12,"category":"Abteilung","label":"asdhjasdhfsa","cssClass":"tagcat1","creationDate":"May 18, 2018 12:44:21 PM","deletionDate":"May 18, 2018 12:45:50 PM"},
+    {"id":2,"category":"Abteilung","label":"Buchhaltung","cssClass":"tagcat1","creationDate":"Mar 8, 2018 2:45:30 PM","deletionDate":"May 18, 2018 11:20:44 AM"},
+    {"id":3,"category":"Abteilung","label":"Intern","cssClass":"tagcat1","creationDate":"Mar 8, 2018 2:45:30 PM"},
+    {"id":4,"category":"Abteilung","label":"Manufaktur","cssClass":"tagcat1","creationDate":"Mar 8, 2018 2:45:30 PM"},
+    {"id":1,"category":"Abteilung","label":"Vertrieb","cssClass":"tagcat1","creationDate":"Mar 8, 2018 2:45:30 PM"},
+    {"id":11,"category":"Calendar","label":"Corporate","cssClass":"tagcat4","creationDate":"May 15, 2018 10:29:08 PM","deletionDate":"May 18, 2018 9:08:55 AM"},
+    {"id":10,"category":"Kategorie","label":"Ablage","cssClass":"tagcat3","creationDate":"Mar 8, 2018 2:45:30 PM"},
+    {"id":9,"category":"Kategorie","label":"DRINGEND","cssClass":"tagcat3","creationDate":"Mar 8, 2018 2:45:30 PM"},
+    {"id":8,"category":"Kategorie","label":"WICHTIG","cssClass":"tagcat3","creationDate":"Mar 8, 2018 2:45:30 PM"},
+    {"id":5,"category":"Kunde","label":"Heideldruck","cssClass":"tagcat2","creationDate":"Mar 8, 2018 2:45:30 PM"},
+    {"id":6,"category":"Kunde","label":"LidlSchwarz","cssClass":"tagcat2","creationDate":"Mar 8, 2018 2:45:30 PM"},
+    {"id":7,"category":"BOSS","label":"Kaufland","cssClass":"tagcat2","creationDate":"Mar 8, 2018 2:45:30 PM"}]
+}
+
+
 $("div#Eingabe").toggle();
 /* Ajax zum daten runterladen der JsonEvents 
 $(document).ready(function () {
@@ -45,9 +63,14 @@ function FehlerdatenLaden() {
 }
 /* Rein laden der Tags in das Formular */
 
+/*Laden der Tags zum auswählen */
 function fillStandardFormular() {
-    for(var tags in loadData("jsontags.jsp")) {
+   /* for(var tags in loadData("jsontags.jsp")) {
         document.getElementById("tags").innerHTML += "<option>" + tags +"</option>";
+    }
+    */
+     for(TAG in tagArray.tags) {
+        document.getElementById("tags").innerHTML += "<option>" + tagArray.tags[TAG].label +"</option>";
     }
 }
 
---
+++ jsontags.jsp
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{ "status": "ok", "tags":[{"id":12,"category":"Abteilung","label":"asdhjasdhfsa","cssClass":"tagcat1","creationDate":"May 18, 2018 12:44:21 PM","deletionDate":"May 18, 2018 12:45:50 PM"},{"id":2,"category":"Abteilung","label":"Buchhaltung","cssClass":"tagcat1","creationDate":"Mar 8, 2018 2:45:30 PM","deletionDate":"May 18, 2018 11:20:44 AM"},{"id":3,"category":"Abteilung","label":"Intern","cssClass":"tagcat1","creationDate":"Mar 8, 2018 2:45:30 PM"},{"id":4,"category":"Abteilung","label":"Manufaktur","cssClass":"tagcat1","creationDate":"Mar 8, 2018 2:45:30 PM"},{"id":1,"category":"Abteilung","label":"Vertrieb","cssClass":"tagcat1","creationDate":"Mar 8, 2018 2:45:30 PM"},{"id":11,"category":"Calendar","label":"Corporate","cssClass":"tagcat4","creationDate":"May 15, 2018 10:29:08 PM","deletionDate":"May 18, 2018 9:08:55 AM"},{"id":10,"category":"Kategorie","label":"Ablage","cssClass":"tagcat3","creationDate":"Mar 8, 2018 2:45:30 PM"},{"id":9,"category":"Kategorie","label":"DRINGEND","cssClass":"tagcat3","creationDate":"Mar 8, 2018 2:45:30 PM"},{"id":8,"category":"Kategorie","label":"WICHTIG","cssClass":"tagcat3","creationDate":"Mar 8, 2018 2:45:30 PM"},{"id":5,"category":"Kunde","label":"Heideldruck","cssClass":"tagcat2","creationDate":"Mar 8, 2018 2:45:30 PM"},{"id":6,"category":"Kunde","label":"LidlSchwarz","cssClass":"tagcat2","creationDate":"Mar 8, 2018 2:45:30 PM"},{"id":7,"category":"Kunde","label":"ReweKaufland","cssClass":"tagcat2","creationDate":"Mar 8, 2018 2:45:30 PM"}]
+}
+
